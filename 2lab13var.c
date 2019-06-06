2970897d6b59a05028ff6de50cf820ea7f37c347
Nick Wynther <32970514+nekitkee@users.noreply.github.com>
2019-06-06 16:54:46 +0300

    2lab13var

---
+++ 2lab13var.c
@@ -0,0 +1,81 @@
+/*
+ * 2lab13var.c
+ *
+ * Created: 06.06.2019 15:30:11
+ * Author : nekit
+ */ 
+
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define SW0 0b11111110
+#define SW1 0b11111101
+#define SW2 0b11111011
+
+volatile char mode=2;
+volatile char temp =0;
+
+ISR(PCINT0_vect) {
+		
+	if (mode == 2){
+		
+		
+	if (PINB == SW1 ) {
+		mode = 1; //off
+	}
+	
+	if (PINB == SW2) {//on
+		mode = 0;
+		
+	}
+	_delay_ms(600);
+	//cli();
+	}
+}
+
+
+
+int main(void)
+{
+	
+	DDRB = 0x00;
+	PORTB = 0xFF;
+
+	DDRD = 0xFF;
+	PORTD = 0x00;
+		
+	
+
+	PCICR|=(1<<PCIE0);
+	PCMSK0=0b00000110;
+	
+	
+sei();
+
+
+    while (1) 
+    {
+		
+		
+		if(mode==1 && PINB!= 0xFF ) { 
+			
+			PORTD = PORTD | (~(PINB));
+			_delay_ms(300);
+			mode=2;
+			//sei();
+		}
+	
+		if (mode==0 && PINB != 0xFF) { 
+			
+			
+			PORTD &= PINB;
+			_delay_ms(300);
+			mode=2;
+			//sei();
+		}
+    }
+}
+
